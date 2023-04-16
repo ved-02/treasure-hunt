07b242449202b1bfd3f4f5820057c17537841ebc
ved-02 <76402029+ved-02@users.noreply.github.com>
2023-04-16 16:26:47 +0530

    add build

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ async function dbConnect() {
 app.use("/api/account", accountRoute);
 app.use("/api/detail", detailRoute);
 app.use("/api/admin", adminRoute);
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./build")));
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "./build/index.html"));
 })
